c4f291dd5f3d726ac70970ec445f884d4dfb97db
leahsteinberg <leahsteinberg1@gmail.com>
2014-04-07 14:27:50 -0400

    trying to count matches of strings

--- example.js
+++ example.js
@@ -3,7 +3,8 @@ var cheerio = require('cheerio');
 console.log('sjkfjlsd1');
 var url =  'http://www.yelp.com/biz/lali-restaurant-new-york';
 console.log('2');
-
+var our_text = ' ';
+var old_news_count = [];
 
 request(url, function(error, response, html){
 
@@ -11,10 +12,21 @@ request(url, function(error, response, html){
     var $ = cheerio.load(html);
     $('p.review_comment').each(function(i, element){
       var a = $(this);
-		console.log(a.text());
+	      //console.log(a.text());
+		our_text = (a.text());
+		old_news_count = our_text.match(/packed|location|classic|line|institution|list|tourist/g);
+	console.log("old news count is:");
+	console.log(old_news_count);
+	    console.log(old_news_count.length);
     });
   }
 });
+console.log(our_text)
+// var old_news = ['packed', 'location', 'classic', 'line', 'institution', 'list', 'tourist', 'to do'];
+// var undiscovered = ['authentic', 'hidden', 'hole in the wall', 'hole-in-the-wall', 'homemade', 'secret', 'gem', 'undiscovered', 'intimate', 'unique', 'different'];
+// var introverted = ['home', 'polite', 'welcoming', 'homemade', 'gourmet', 'date', 'ambience', 'intimate', 'quiet'];
+// var extroverted = ['fucking', 'people watching', 'dj', 'DJ', 'upbeat', 'happy hour', 'drunk', 'party', 'loud', 'crowd'];
+
 
 //request(url);
 
